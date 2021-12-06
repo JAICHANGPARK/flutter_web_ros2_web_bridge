11ff27748d4cda9a90b814a899cbaa22c0786d43
JAICHANGPARK <aristojeff@gmail.com>
2021-12-06 13:30:53 +0900

    Update ros2_pub_sub.js

--- web/ros2_pub_sub.js
+++ web/ros2_pub_sub.js
@@ -7,10 +7,6 @@
   // If there is an error on the backend, an 'error' emit will be emitted.
   ros.on('error', function(error) {
 
-<!--    document.getElementById('connecting').style.display = 'none';-->
-<!--    document.getElementById('connected').style.display = 'none';-->
-<!--    document.getElementById('closed').style.display = 'none';-->
-<!--    document.getElementById('error').style.display = 'inline';-->
     console.log(error);
   var obj = new Object();
    obj.connecting  = false;
@@ -36,10 +32,6 @@
        console.log('Connection made!' + jsonString);
          window.parent.postMessage(jsonString, "*");
 
-<!--    document.getElementById('connecting').style.display = 'none';-->
-<!--    document.getElementById('error').style.display = 'none';-->
-<!--    document.getElementById('closed').style.display = 'none';-->
-<!--    document.getElementById('connected').style.display = 'inline';-->
   });
 
   ros.on('close', function() {
@@ -52,9 +44,6 @@
            obj.error  = "";
            var jsonString= JSON.stringify(obj);
              window.parent.postMessage(jsonString, "*");
-<!--    document.getElementById('connecting').style.display = 'none';-->
-<!--    document.getElementById('connected').style.display = 'none';-->
-<!--    document.getElementById('closed').style.display = 'inline';-->
   });
 
   // Create a connection to the rosbridge WebSocket server.
@@ -73,17 +62,15 @@
     example.publish({data: message});
     if(isConnected){
      console.log('publish' + message);
-         window.parent.postMessage('publish' + message, "*");
+//         window.parent.postMessage('publish' + message, "*");
     }else{
        console.log("Not Connected");
     }
 
-<!--    document.getElementById("publisher").innerText = message;-->
   }, 1000);
 
   // Subscribe a Topic
   example.subscribe(function(message) {
-   console.log('subscribe');
-   window.parent.postMessage('any message', "*");
-<!--    document.getElementById("subscription").innerText = message.data-->
+   console.log('subscribe'+ message);
+   window.parent.postMessage(""+message.data, "*");
   });
